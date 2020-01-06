71aec02de4280ae9eb4afd8f1230ab135b524940
jeremyt135 <jdt13579@gmail.com>
2020-01-06 12:08:06 -0800

    Rename main.js to index.js for webpack

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import { Game } from './game/game'
+
+$(document).ready(() => {
+  main()
+})
+
+function main() {
+  try {
+    const game = new Game()
+  } catch (error) {
+    console.error(error)
+  }
+}
