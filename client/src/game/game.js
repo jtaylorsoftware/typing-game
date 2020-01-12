c519292fd17255db1bf20588912b4a715e885f63
jeremyt135 <jdt13579@gmail.com>
2020-01-11 17:23:44 -0800

    Refactor code
    Changed order of method definitions to flow better and increase locality

--- client/src/game/game.js
+++ client/src/game/game.js
@@ -14,7 +14,7 @@ export class Game {
     this.modeInfo = $('.mode__info')
 
     this.MAX_TARGETS = 4
-    this.MIN_TIMEREQUIRED = 4 // time for target to reach bottom in seconds
+    this.MIN_TIMEREQUIRED = 5 // min time for target to reach bottom in seconds
     this.TARGET_GOAL = 90
 
     this.GAMEPLAY_TIME = 1 // minutes
@@ -56,67 +56,6 @@ export class Game {
     this.step = this.step.bind(this)
   }
 
-  async reset() {
-    this.timeCounter = new DeltaTimeCounter()
-    this.clock.setTime(this.GAMEPLAY_TIME)
-
-    this.numTargets = 0
-    this.targetMap.clear()
-    this.wordCache.clear()
-    await this.getRandomWords(100)
-    if (this.wordCache.size === 0) {
-      throw Error('Could not reach server')
-    }
-
-    this.target = null
-    this.freeColumns = Array.from({ length: 4 }, (v, i) => i + 1)
-    console.log(this.freeColumns)
-    this.clearInput()
-
-    this.score = 0
-    this.scoreCounter = $('.score')
-    this.scoreCounter.html(this.score)
-
-    this.life = 100
-    this.lifeCounter = $('.life__points')
-    this.lifeCounter.html(this.life)
-
-    this.frame = 0
-
-    this.gameOver = false
-    this.stopped = false
-  }
-
-  haveTarget() {
-    return this.target !== null
-  }
-
-  /**
-   * Gets the first jquery object that has data-first attribute set to letter
-   * @param {string} letter the letter to use when finding the target
-   */
-  selectTargetFromLetter(letter) {
-    // console.log(letter)
-    const targetWithLetter = this.targetMap.get(letter)
-    return targetWithLetter
-  }
-
-  selectTarget() {
-    // select a new target if there isn't one
-    const target = this.selectTargetFromLetter(
-      this.getGameInput()
-        .slice(0, 1)
-        .toLowerCase()
-    )
-    // console.log(target)
-    if (target) {
-      this.target = target
-    } else {
-      // no target, dont display the new text
-      this.setGameInput(this.currentInput)
-    }
-  }
-
   async getRandomWords(count) {
     try {
       const res = await fetch(`/api/word?count=${count}`)
@@ -136,95 +75,6 @@ export class Game {
     return next
   }
 
-  /**
-   * Appends a target to the target area
-   */
-  async createTarget() {
-    // console.log('create target')
-    try {
-      let word = await this.getWordFromCache()
-      while (this.targetMap.has(word[0])) {
-        word = await this.getWordFromCache()
-      }
-      const column = this.freeColumns.shift()
-      const target = new Target(
-        word,
-        column,
-        0,
-        this.TARGET_GOAL,
-        Math.max(
-          this.MIN_TIMEREQUIRED,
-          randomInt(this.MIN_TIMEREQUIRED * word.length)
-        )
-      )
-      this.targetArea.append(target.root)
-      target.onGoalReached = this.targetReachedGoal.bind(this)
-      this.targetMap.set(word[0], target)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  destroyTarget() {
-    this.targetMap.delete(this.target)
-    this.freeColumns.push(this.target.column)
-    this.target.remove()
-    this.target = null
-    this.numTargets -= 1
-  }
-
-  /**
-   * Directs user's input toward the current target
-   */
-  attackTarget() {
-    if (!this.target) {
-      return
-    }
-    const targetText = this.target.getText()
-    const gameInputText = this.getGameInput().toLowerCase()
-    if (gameInputText === targetText) {
-      // destroy the target if the user finished typing the target word
-      this.clearInput()
-      this.setScore(this.score + targetText.length)
-      this.destroyTarget()
-    } else {
-      // check the progress against the target word
-      const nextInput = this.getGameInput().toLowerCase()
-      if (!targetText.startsWith(nextInput)) {
-        // block user's input if it's not part of the word
-        this.blockInput()
-      } else {
-        // accept matching input
-        this.currentInput = nextInput
-        this.target.setProgress(this.currentInput)
-      }
-    }
-  }
-
-  /**
-   * Updates target positions in sync with framerate
-   * @param {number} deltaTime the time in milliseconds since last frame
-   */
-  updateTargets(deltaTime) {
-    // console.log(this.targetMap[Symbol.iterator]())
-    for (const target of this.targetMap.values()) {
-      if (target.removed) {
-        this.targetMap.delete(target)
-      } else {
-        target.update(deltaTime)
-      }
-    }
-  }
-
-  onGameOver() {
-    this.gameOver = true
-    this.paused = true
-    this.modeInfo.addClass('mode__info--hidden')
-    this.gameInput.addClass('game-input--hidden')
-    $('#menuScore').html(this.score)
-    $('#gameOverMenu').toggleClass('menu--hidden')
-  }
-
   setLife(life) {
     this.life = Math.max(0, life)
     this.lifeCounter.html(this.life)
@@ -233,28 +83,9 @@ export class Game {
     }
   }
 
-  targetReachedGoal(target) {
-    const word = target.getText()
-    console.log(target, word)
-    this.numTargets -= 1
-    this.freeColumns.push(target.column)
-    this.setLife(this.life - word.length)
-    if (word.startsWith(this.currentInput)) {
-      this.target = null
-      this.clearInput()
-    }
-  }
-
   setScore(score) {
     this.score = score
     this.scoreCounter.html(this.score)
-  }
-
-  /**
-   * Prevent user input from updating by setting the gameInput value to the previous input
-   */
-  blockInput() {
-    this.setGameInput(this.currentInput)
   }
 
   setGameInput(str) {
@@ -268,6 +99,13 @@ export class Game {
   clearInput() {
     this.setGameInput('')
     this.currentInput = ''
+  }
+
+  /**
+   * Prevent user input from updating by setting the gameInput value to the previous input
+   */
+  blockInput() {
+    this.setGameInput(this.currentInput)
   }
 
   /**
@@ -303,6 +141,128 @@ export class Game {
     this.attackTarget()
   }
 
+  haveTarget() {
+    return this.target !== null
+  }
+
+  targetReachedGoal(target) {
+    const word = target.getText()
+    console.log(target, word)
+    this.numTargets -= 1
+    this.freeColumns.push(target.column)
+    this.setLife(this.life - word.length)
+    if (word.startsWith(this.currentInput)) {
+      this.target = null
+      this.clearInput()
+    }
+  }
+
+  /**
+   * Gets the first jquery object that has data-first attribute set to letter
+   * @param {string} letter the letter to use when finding the target
+   */
+  selectTargetFromLetter(letter) {
+    // console.log(letter)
+    const targetWithLetter = this.targetMap.get(letter)
+    return targetWithLetter
+  }
+
+  selectTarget() {
+    // select a new target if there isn't one
+    const target = this.selectTargetFromLetter(
+      this.getGameInput()
+        .slice(0, 1)
+        .toLowerCase()
+    )
+    // console.log(target)
+    if (target) {
+      this.target = target
+    } else {
+      // no target, dont display the new text
+      this.setGameInput(this.currentInput)
+    }
+  }
+
+  /**
+   * Directs user's input toward the current target
+   */
+  attackTarget() {
+    if (!this.target) {
+      return
+    }
+    const targetText = this.target.getText()
+    const gameInputText = this.getGameInput().toLowerCase()
+    if (gameInputText === targetText) {
+      // destroy the target if the user finished typing the target word
+      this.clearInput()
+      this.setScore(this.score + targetText.length)
+      this.destroyTarget()
+    } else {
+      // check the progress against the target word
+      const nextInput = this.getGameInput().toLowerCase()
+      if (!targetText.startsWith(nextInput)) {
+        // block user's input if it's not part of the word
+        this.blockInput()
+      } else {
+        // accept matching input
+        this.currentInput = nextInput
+        this.target.setProgress(this.currentInput)
+      }
+    }
+  }
+
+  destroyTarget() {
+    this.targetMap.delete(this.target)
+    this.freeColumns.push(this.target.column)
+    this.target.remove()
+    this.target = null
+    this.numTargets -= 1
+  }
+
+  /**
+   * Appends a target to the target area
+   */
+  async createTarget() {
+    // console.log('create target')
+    try {
+      let word = await this.getWordFromCache()
+      while (this.targetMap.has(word[0])) {
+        word = await this.getWordFromCache()
+      }
+      const column = this.freeColumns.shift()
+      const target = new Target(
+        word,
+        column,
+        0,
+        this.TARGET_GOAL,
+        Math.max(
+          this.MIN_TIMEREQUIRED,
+          randomInt(this.MIN_TIMEREQUIRED * word.length)
+        )
+      )
+      this.targetArea.append(target.root)
+      target.onGoalReached = this.targetReachedGoal.bind(this)
+      this.targetMap.set(word[0], target)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  /**
+   * Updates target positions in sync with framerate
+   * @param {number} deltaTime the time in milliseconds since last frame
+   */
+  updateTargets(deltaTime) {
+    // console.log(this.targetMap[Symbol.iterator]())
+    for (const target of this.targetMap.values()) {
+      if (target.removed) {
+        this.targetMap.delete(target)
+      } else {
+        target.update(deltaTime)
+      }
+    }
+  }
+
   /**
    * Do framerate dependent update of any game logic and movement
    * @param {number} deltaTime the time since last frame in milliseconds
@@ -326,6 +286,16 @@ export class Game {
       // this.stop()
     }
   }
+
+  onGameOver() {
+    this.gameOver = true
+    this.paused = true
+    this.modeInfo.addClass('mode__info--hidden')
+    this.gameInput.addClass('game-input--hidden')
+    $('#menuScore').html(this.score)
+    $('#gameOverMenu').toggleClass('menu--hidden')
+  }
+
   start() {
     this.reset()
       .then(() => {
@@ -337,6 +307,38 @@ export class Game {
       })
       .catch(error => console.log(error))
   }
+
+  async reset() {
+    this.timeCounter = new DeltaTimeCounter()
+    this.clock.setTime(this.GAMEPLAY_TIME)
+
+    this.numTargets = 0
+    this.targetMap.clear()
+    this.wordCache.clear()
+    await this.getRandomWords(100)
+    if (this.wordCache.size === 0) {
+      throw Error('Could not reach server')
+    }
+
+    this.target = null
+    this.freeColumns = Array.from({ length: 4 }, (v, i) => i + 1)
+    console.log(this.freeColumns)
+    this.clearInput()
+
+    this.score = 0
+    this.scoreCounter = $('.score')
+    this.scoreCounter.html(this.score)
+
+    this.life = 100
+    this.lifeCounter = $('.life__points')
+    this.lifeCounter.html(this.life)
+
+    this.frame = 0
+
+    this.gameOver = false
+    this.stopped = false
+  }
+
   /**
    * Do one full simulation step of the game including input, updates, and requesting redraw
    */
